71e78828553b6eefd382a811ef282af4c308e439
All PGS 모든 프로그래머들 <kms0219kms@naver.com>
2020-08-31 20:11:08 +0900

    Update index.js

--- index.js
+++ index.js
@@ -11,31 +11,6 @@ dotenv.config({
     path: './.env'
 });
 
-const http = require('http');
-const url = require('url');
-http.createServer((request, response) => {
-  const path = url.parse(request.url, true).pathname; // url에서 path 추출
-  if (request.method === 'GET') { // GET 요청이면
-    if (path === '/') { // 주소가 /이면
-      response.writeHead(200,{'Content-Type':'text/html'});
-      fs.readFile(__dirname + '/public/index.html', (err, data) => {
-        if (err) {
-          return console.error(err);
-        }
-        response.end(data, 'utf-8');
-      });
-    } else { // 매칭되는 주소가 없으면
-      response.writeHead(404,{'Content-Type':'text/html'});
-      fs.readFile(__dirname + '/public/404.html', (err, data) => {
-        if (err) {
-          return console.error(err);
-        }
-        response.end(data, 'utf-8');
-    })
-  }
-}
-}).listen(8080);
-
 client.on('ready', () => {
   console.log(`코로나봇 준비 완료!`);
   client.user.setActivity('도움말: !help 또는 !도움말 | COVID-19 BOT', { type: 'WATCHING' })
